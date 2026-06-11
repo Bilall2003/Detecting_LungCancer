8743e60a17d4930247987a19d1fb1b67914dc7a8
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-06-11 12:37:59 +0000

    update code

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
 from src.main import func
 
-image=st.file_uploader("Upload image",help="only one accepted at a time")
-fig, img, smooth, enhanced, mask, area, perimeter, ecc = func(image)
+image_path = "asset/LungCancerCTscan.jpg"
+
+fig, img, smooth, enhanced, mask, area, perimeter, ecc = func(image_path)
 
 st.pyplot(fig)
 
-st.subheader("Features Extracted")
-st.write("Area:", area)
-st.write("Perimeter:", perimeter)
-st.write("Eccentricity:", ecc)
+st.subheader("Extracted Features")
+
+st.write("Area", area)
+st.write("Perimeter", round(perimeter, 2))
+st.write("Eccentricity", round(ecc, 4))
