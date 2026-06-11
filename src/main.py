74c53baf568ec0cc457dfedec70075030e9edf7b
Bilal Ahmed <bilallahmedd685@gmail.com>
2026-06-11 11:16:09 +0000

    update code

--- src/main.py
+++ src/main.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 
 def func():
     
-    img=cv.imread("asset/LungCancerCTscan.jpg")
+    img=cv.imread("asset/LungCancerCTscan.jpg", cv.IMREAD_GRAYSCALE)
     
-    img_rgb=cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    # img_rgb=cv.cvtColor(img, cv.COLOR_BGR2RGB)
     
     fig,ax=plt.subplots()
-    ax.imshow(img_rgb)
+    ax.imshow(img,cmap="gray")
     ax.axis("off")
     
     return fig
+    
+    
     
